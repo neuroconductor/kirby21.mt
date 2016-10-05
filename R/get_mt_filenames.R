567e48083dc4fb164cd415e3445bfea44fc52617
muschellij2 <muschellij2@gmail.com>
2016-10-05 01:10:49 -0400

    adding files to repo

---
+++ R/get_mt_filenames.R
@@ -0,0 +1,15 @@
+#' @title Get MT Image Filenames
+#'
+#' @description Return the filenames for the MT images
+#' @param ... arguments to pass to \code{\link{get_image_filenames}},
+#' \code{modalities = "MT"} so it cannot be specified
+#' @return Vector fo filenames
+#' 
+#' @examples
+#' get_mt_filenames()
+#' @export
+#' @importFrom kirby21.base get_image_filenames
+get_mt_filenames = function(...) {
+  x = kirby21.base::get_image_filenames(modalities = "MT", ...)
+  return(x)
+}
