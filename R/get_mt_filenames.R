ab4088f0cdba39b90a8589a94c40cbfe25427f61
muschellij2 <muschellij2@gmail.com>
2018-02-01 11:07:03 -0500

    updated description with doi

--- R/get_mt_filenames.R
+++ R/get_mt_filenames.R
@@ -3,7 +3,7 @@
 #' @description Return the filenames for the MT images
 #' @param ... arguments to pass to \code{\link{get_image_filenames}},
 #' \code{modalities = "MT"} so it cannot be specified
-#' @return Vector fo filenames
+#' @return Vector of filenames
 #' 
 #' @examples
 #' get_mt_filenames()
